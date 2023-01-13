75edff5ad660d306c0644175ea5ab6541b6aab16
beansisthe1 <122555448+beansisthe1@users.noreply.github.com>
2023-01-13 12:05:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Giraffe)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Giraffe)
+})
+basic.showIcon(IconNames.Chessboard)
