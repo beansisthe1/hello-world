1e35c85549d40d9116b258c7d7d297bd9dcdbb68
beansisthe1 <122555448+beansisthe1@users.noreply.github.com>
2023-01-13 12:06:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Giraffe)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Giraffe)
+    basic.showIcon(IconNames.Ghost)
 })
 basic.showIcon(IconNames.Chessboard)
