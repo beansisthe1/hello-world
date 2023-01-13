be45667096f31a44df20de2870ab60542fab8449
beansisthe1 <122555448+beansisthe1@users.noreply.github.com>
2023-01-13 12:17:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Giraffe)
 })
-input.onGesture(Gesture.ScreenUp, function () {
-    basic.showIcon(IconNames.Happy)
-})
-input.onGesture(Gesture.ScreenDown, function () {
-    basic.showIcon(IconNames.Sad)
-})
-input.onButtonPressed(Button.AB, function () {
-    music.playMelody("C C E E G G B B ", 120)
-})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Ghost)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playMelody("C C E E G G B B ", 120)
 })
 basic.showIcon(IconNames.Chessboard)
